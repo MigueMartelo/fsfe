ff6be8e6d571ae14fb7b8433d9a57c966cde343f
Miguel Martelo <mmartelo@farmmutualre.com>
2024-08-04 11:57:07 -0500

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,16 @@ server.listen(3000, function () {
   console.log("server staterd on port 3000");
 });
 
+process.on("SIGINT", () => {
+  console.log("sigint");
+  wss.clients.forEach((client) => {
+    client.close();
+  });
+  server.close(() => {
+    shutDownDB();
+  });
+});
+
 /** Begin Websocket */
 const webSocketServer = require("ws").Server;
 
@@ -26,6 +36,10 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server!");
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+  `);
+
   ws.on("close", function close() {
     wss.broadcast(`Current visitors: ${numClients}`);
     console.log("A client has disconnected");
@@ -37,3 +51,30 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+// End Websocket
+
+// Begin database
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutDownDB() {
+  getCounts();
+  console.log("Shutting down database");
+  db.close();
+}
